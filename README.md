4b9afa8714728b24a5e8d2bbcb22da2b27ee062b
Bytzo <bytzo@bytzo.net>
2022-06-25 21:46:22 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MC-2215
+
+A Minecraft Fabric mod to fix MC-2215. See [https://bugs.mojang.com/browse/MC-2215](https://bugs.mojang.com/browse/MC-2215) for more information.
---
+++ src/main/java/net/bytzo/mc_2215/mixins/SettingsMixin.java
@@ -0,0 +1,45 @@
+package net.bytzo.mc_2215.mixins;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.server.dedicated.Settings;
+
+@Mixin(Settings.class)
+public class SettingsMixin {
+	@Unique
+	private static final Charset CHARSET = StandardCharsets.UTF_8;
+
+	@Redirect(
+			method = "loadFromFile(Ljava/nio/file/Path;)Ljava/util/Properties;",
+			at = @At(
+					value = "INVOKE",
+					target = "Ljava/util/Properties;load(Ljava/io/InputStream;)V"
+			)
+	)
+	private static void load(Properties properties, InputStream inStream) throws IOException {
+		properties.load(new InputStreamReader(inStream, CHARSET));
+	}
+	
+	@Redirect(
+			method = "store(Ljava/nio/file/Path;)V",
+			at = @At(
+					value = "INVOKE",
+					target = "Ljava/util/Properties;store(Ljava/io/OutputStream;Ljava/lang/String;)V"
+			)
+	)
+	private void store(Properties properties, OutputStream out, String comments) throws IOException {
+		properties.store(new OutputStreamWriter(out, CHARSET), comments);
+	}
+}
